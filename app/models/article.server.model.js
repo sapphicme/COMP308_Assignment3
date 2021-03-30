bcda3ab96c0d6c9e8f062c9075693a0e1ce28697
Arielle Mueller <43253070+sapphicme@users.noreply.github.com>
2021-03-29 20:01:28 -0400

    Skeleton

---
+++ app/models/article.server.model.js
@@ -0,0 +1,31 @@
+﻿const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const ArticleSchema = new Schema({
+    created: {
+        type: Date,
+        default: Date.now
+    },
+    coursecode: {
+        type: String,
+        default: '',
+        trim: true,
+        required: 'Course Code cannot be blank'
+    },
+    coursename: {
+        type: String, default: '',
+        trim: true
+    },
+    section: {
+        type: Number, default: '',
+        trim: true
+    },
+    semester: {
+        type: Number, default: '',
+        trim: true
+    },
+    creator: {
+        type: Schema.ObjectId,
+        ref: 'User'
+    }
+});
+mongoose.model('Article', ArticleSchema);
