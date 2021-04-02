908e26bc4bce0899db99e5d200e479f2eac7c4db
Arielle Mueller <43253070+sapphicme@users.noreply.github.com>
2021-04-02 17:36:56 -0400

    adding front end stuff

--- app/controllers/users.server.controller.js
+++ app/controllers/users.server.controller.js
@@ -122,23 +122,15 @@ exports.authenticate = function(req, res, next) {
 				return next(err);
 			} else {
 			console.log(user)
-			//compare passwords	
 			if(bcrypt.compareSync(password, user.password)) {
-				// Create a new token with the user id in the payload
-  				// and which expires 300 seconds after issue
 				const token = jwt.sign({ id: user._id, studentnumber: user.studentnumber }, jwtKey, 
 					{algorithm: 'HS256', expiresIn: jwtExpirySeconds });
 				console.log('token:', token)
-				// set the cookie as the token string, with a similar max age as the token
-				// here, the max age is in milliseconds
 				res.cookie('token', token, { maxAge: jwtExpirySeconds * 1000,httpOnly: true});
 				res.status(200).send({ screen: user.studentnumber });
-				//
-				//res.json({status:"success", message: "user found!!!", data:{user:
-				//user, token:token}});
-				
+		
 				req.user=user;
-				//call the next middleware
+				
 				next()
 			} else {
 				res.json({status:"error", message: "Invalid student number/password!!!",
@@ -149,32 +141,22 @@ exports.authenticate = function(req, res, next) {
 		
 	});
 };
-//
-// protected page uses the JWT token
 exports.welcome = (req, res) => {
-	// We can obtain the session token from the requests cookies,
-	// which come with every request
 	const token = req.cookies.token
 	console.log(token)
-	// if the cookie is not set, return an unauthorized error
-	if (!token) {
+		if (!token) {
 	  return res.status(401).end()
 	}
   
 	var payload;
 	try {
-	  // Parse the JWT string and store the result in `payload`.
-	  // Note that we are passing the key in this method as well. This method will throw an error
-	  // if the token is invalid (if it has expired according to the expiry time we set on sign in),
-	  // or if the signature does not match
+	  
 	  payload = jwt.verify(token, jwtKey)
 	} catch (e) {
 	  if (e instanceof jwt.JsonWebTokenError) {
-		// if the error thrown is because the JWT is unauthorized, return a 401 error
 		return res.status(401).end()
 	  }
-	  // otherwise, return a bad request error
-	  return res.status(400).end()
+	   return res.status(400).end()
 	}
   
 	// Finally, return the welcome message to the user, along with their
